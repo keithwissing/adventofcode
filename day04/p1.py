06bdd3bc0f9c650de8ef7385070ea874a3765c89
Keith Wissing <hitiek@xorad.com>
2015-12-04 10:09:15 -0500

    day 4

---
+++ day04/p1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import hashlib
+
+def hash_the_string(input):
+    m = hashlib.md5()
+    m.update(input)
+    return m.hexdigest()
+
+def zeros(input):
+    h = hash_the_string(input)
+    return len(h) - len(h.lstrip('0'))
+
+def mine(key,nz,hint=1):
+    """
+    >>> mine("abcdef",5,600000)
+    609043
+    """
+    count = hint
+    while True:
+        test = key+str(count)
+        if zeros(test) >= nz:
+            break
+        count = count + 1
+    print count
+
+def main():
+    mine("yzbqklnj",5)
+    mine("yzbqklnj",6)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
+
