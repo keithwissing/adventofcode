a8bfdeafedf50ece3f942de846e11157ecf89705
Keith Wissing <hitiek@xorad.com>
2015-12-08 15:03:18 -0500

    day 8

---
+++ day08/aoc_day08.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+def string_size_difference(lines):
+    """
+    >>> string_size_difference( [\
+        '""',\
+        '"abc"',\
+        '"aaa\\\\"aaa"',\
+        '"\\\\x27"' ] )
+    12
+    """
+    pass
+
+def part2(lines):
+    """
+    >>> part2( [\
+        '""',\
+        '"abc"',\
+        '"aaa\\\\"aaa"',\
+        '"\\\\x27"' ] )
+    19
+    """
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
+
---
+++ day08/p1.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+import re
+
+def mem_len(line):
+    step = line[1:len(line)-1]
+    step = step.replace("\\\\", "\\")
+    step = step.replace("\\\"", "\"")
+    step = re.sub(r"\\x[0-9a-f][0-9a-f]", "_", step)
+    #print len(line),len(step),line,step
+    return len(step)
+
+def string_size_difference(lines):
+    """
+    >>> string_size_difference( [\
+        '""',\
+        '"abc"',\
+        '"aaa\\\\"aaa"',\
+        '"\\\\x27"' ] )
+    12
+    """
+    tlen = 0
+    tmlen = 0
+    for line in lines:
+        tlen = tlen + len(line)
+        tmlen = tmlen + mem_len(line)
+    return tlen - tmlen
+
+def code_encode(line):
+    step = line
+    step = step.replace("\\", "\\\\")
+    step = step.replace("\"", "\\\"")
+    step = '"'+step+'"'
+    #print len(line),len(step),line,step
+    return len(step)
+
+def part2(lines):
+    """
+    >>> part2( [\
+        '""',\
+        '"abc"',\
+        '"aaa\\\\"aaa"',\
+        '"\\\\x27"' ] )
+    19
+    """
+    tlen = 0
+    tmlen = 0
+    for line in lines:
+        tlen = tlen + len(line)
+        tmlen = tmlen + code_encode(line)
+    return tmlen - tlen
+
+def main():
+    input = [line.strip() for line in open('input.txt')]
+    print string_size_difference(input)
+    print part2(input)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
+
