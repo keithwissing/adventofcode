19b9f8323d4d1829e5e162c0bf8b86dc1a0d353f
Keith Wissing <hitiek@xorad.com>
2017-12-07 01:00:43 -0500

    Completed day 7

---
+++ 2017/day07.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+
+def parse_input_line(line):
+    """
+    >>> parse_input_line("pbga (66)")
+    ('pbga', 66, [])
+    >>> parse_input_line("fwft (72) -> ktlj, cntj, xhth")
+    ('fwft', 72, ['ktlj', 'cntj', 'xhth'])
+    """
+    a = line.split(" ")
+    name = a[0]
+    weight = int(a[1].lstrip("(").rstrip(")"))
+    if len(a) > 2:
+        c = [x.rstrip(",") for x in a[3:]]
+    else:
+        c = []
+    return (name, weight, c)
+
+def find_bottom(programs):
+    non = []
+    for x in programs:
+        non.extend(x[2])
+    uniq = [x for x in [n[0] for n in programs] if not x in non]
+    return uniq[0]
+
+def find_incorrect_weight(programs):
+    weights = {}
+    more = True
+    while more:
+        more = False
+        for p in programs:
+            cws = [weights.get(x) for x in p[2]]
+            if len(p[2]) == 0:
+                weights[p[0]] = p[1]
+            elif all(cws):
+                weights[p[0]] = p[1] + sum(cws)
+            else:
+                more = True
+    possibles = []
+    for p in programs:
+        if len(p[2]) > 0:
+            bal = [weights[x] for x in p[2]]
+            if bal.count(bal[0]) != len(bal):
+                ndx = [bal.count(x) for x in bal].index(1)
+                bad = bal[ndx]
+                good = bal[(ndx+1)%len(bal)]
+                tar = next( tp for tp in programs if tp[0] == p[2][ndx] )
+                possibles.append( (good, tar[1] - bad + good) )
+    low = min( x[0] for x in possibles )
+    winner = [x for x in possibles if x[0] == low][0]
+    return winner[1]
+
+def main():
+    puzzle_input = [line.rstrip('\n') for line in open("day07_input.txt")]
+    programs = [parse_input_line(x) for x in puzzle_input]
+    print "Part 1 Answer", find_bottom(programs)
+    print "Part 2 Answer", find_incorrect_weight(programs)
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
