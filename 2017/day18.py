7518dfa10f284f8f951987d1a87385bdd78db4c7
Keith Wissing <keith_wissing@bradycorp.com>
2017-12-18 15:06:28 -0500

    Completed day 18 part 2

--- 2017/day18.py
+++ 2017/day18.py
@@ -47,15 +47,73 @@ def test1(lines):
     instructions = [parse_input_line(line) for line in lines]
     return part1(instructions)
 
+class Program(object):
+    def __init__(self, id, inqueue, outqueue, instructions):
+        self.id = id
+        self.inq = inqueue
+        self.outq = outqueue
+        self.instructions = instructions
+        self.reg = {'p': id}
+        self.ip = 0
+        self.sendcount = 0
+
+    def argval(self, arg):
+        return self.reg.get(arg, 0) if arg.isalpha() else int(arg)
+
+    def run(self):
+        if self.ip >= 0 and self.ip < len(self.instructions):
+            inst = self.instructions[self.ip]
+            val = self.argval(inst[2]) if len(inst) > 2 else 0
+            if inst[0] == "set":
+                self.reg[inst[1]] = val
+            elif inst[0] == "add":
+                self.reg[inst[1]] = self.reg.get(inst[1], 0) + val
+            elif inst[0] == "mul":
+                self.reg[inst[1]] = self.reg.get(inst[1], 0) * val
+            elif inst[0] == "mod":
+                self.reg[inst[1]] = self.reg.get(inst[1], 0) % val
+            elif inst[0] == "snd":
+                self.outq.insert(0, self.argval(inst[1]))
+                self.sendcount += 1
+            elif inst[0] == "rcv":
+                if len(self.inq) > 0:
+                    self.reg[inst[1]] = self.inq.pop()
+                else:
+                    return "wait"
+            elif inst[0] == "jgz":
+                if self.argval(inst[1]) > 0:
+                    self.ip += val - 1
+            self.ip += 1
+            return "run"
+        return "done"
+
+def part2(instructions):
+    queues = [[], []]
+    programs = [Program(0, queues[0], queues[1], instructions), Program(1, queues[1], queues[0], instructions)]
+    status = ["run", "run"]
+    while status.count("run") > 0:
+        last = status[:]
+        status[0] = programs[0].run()
+        status[1] = programs[1].run()
+    return programs[1].sendcount
+
+def test2(lines):
+    """
+    >>> test2(["snd 1","snd 2","snd p","rcv a","rcv b","rcv c","rcv d"])
+    3
+    """
+    instructions = [parse_input_line(line) for line in lines]
+    return part2(instructions)
+
 def main():
     puzzle_input = [line.rstrip('\n') for line in open("day18_input.txt")]
     instructions = [parse_input_line(x) for x in puzzle_input]
     a1 = part1(instructions)
     print "Part 1 Answer", a1
-    # assert a1 == 1547
-    # a2 = part2(instructions)
-    # print "Part 2 Answer", a2
-    # assert a2 == 31154878
+    assert a1 == 2951
+    a2 = part2(instructions)
+    print "Part 2 Answer", a2
+    assert a2 == 7366
 
 if __name__ == '__main__':
     import doctest
