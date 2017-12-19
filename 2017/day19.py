ccaa4437f6bb900c634335a6c27f21ca7b92d3ea
Keith Wissing <hitiek@xorad.com>
2017-12-19 01:19:42 -0500

    Completed day 19

---
+++ 2017/day19.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python
+
+def find_directions(lines, posx, posy):
+    # should probably check for out of bounds coordinates, but this worked for my input
+    dirs = []
+    if lines[posy-1][posx] != ' ':
+        dirs.append('n')
+    if lines[posy+1][posx] != ' ':
+        dirs.append('s')
+    if lines[posy][posx-1] != ' ':
+        dirs.append('w')
+    if lines[posy][posx+1] != ' ':
+        dirs.append('e')
+    return dirs
+
+opposite = {'n': 's', 'e': 'w', 's': 'n', 'w': 'e'}
+
+def part1(lines):
+    (letters, _) = route_packet(lines)
+    return letters
+
+def part2(lines):
+    (_, steps) = route_packet(lines)
+    return steps
+
+def route_packet(lines):
+    posy = 0
+    posx = lines[0].index("|")
+    dir = 's'
+    letters = []
+    stepcount = 0
+    while True:
+        stepcount += 1
+        posy += 1 if dir == 's' else -1 if dir == 'n' else 0
+        posx += 1 if dir == 'e' else -1 if dir == 'w' else 0
+        char = lines[posy][posx]
+        if char == 'F':
+            pass
+        if char.isalpha():
+            letters.append(char)
+        elif char in ['|', '-']:
+            pass
+        elif char == ' ':
+            break
+        elif char == '+':
+            dirs = find_directions(lines, posx, posy)
+            dirs.remove(opposite[dir])
+            dir = dirs[0]
+    return ("".join(letters), stepcount)
+
+def test1():
+    """
+    >>> test1()
+    'ABCDEF'
+    """
+    lines = [
+        '     |          ',
+        '     |  +--+    ',
+        '     A  |  C    ',
+        ' F---|----E|--+ ',
+        '     |  |  |  D ',
+        '     +B-+  +--+ ',
+        '                ']
+    return part1(lines)
+
+def main():
+    lines = [line.rstrip('\n') for line in open("day19_input.txt")]
+    a1 = part1(lines)
+    print "Part 1 Answer", a1
+    assert a1 == 'GEPYAWTMLK'
+    a2 = part2(lines)
+    print "Part 2 Answer", a2
+    assert a2 == 17628
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
