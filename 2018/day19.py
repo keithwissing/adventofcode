8fd5f8b1aa32ddff4becb4890f904bc00df08e86
Keith Wissing <keith_wissing@bradycorp.com>
2018-12-19 12:42:53 -0500

    Year 2018 day 19 part 2

--- 2018/day19.py
+++ 2018/day19.py
@@ -67,11 +67,57 @@ def parse_input(puzzle_input):
         program.append([parts[0], int(parts[1]), int(parts[2]), int(parts[3])])
     return ip, program
 
+def part2(long):
+    """
+    >>> part2(False)
+    1968
+    """
+    init(long)
+    run()
+    return r_A
+
+def run():
+    global r_A, r_B, r_C, r_E, r_F
+    r_F = 1
+    while True:
+        r_C = 1
+        while True:
+            r_B = r_F * r_C
+            if r_B == r_E :
+                r_A = r_F + r_A
+            r_C  = 1 + r_C
+            if r_F * r_C > r_E : # changed condition for optimization
+                r_F = 1 + r_F
+                if r_F > r_E :
+                    return
+                break
+
+def init(long):
+    global r_A, r_B, r_C, r_E, r_F
+    r_A = r_B = r_C = r_E = r_F = 0
+    r_E = r_E + 2
+    r_E = r_E * r_E
+    r_E = 19 * r_E
+    r_E = r_E * 11
+    r_B = 6 + r_B
+    r_B = r_B * 22
+    r_B = 10 + r_B
+    r_E = r_E + r_B
+    if long:
+        r_B = 27
+        r_B = r_B * 28
+        r_B = 29 + r_B
+        r_B = 30 * r_B
+        r_B = r_B * 14
+        r_B = r_B * 32
+        r_E = r_E + r_B
+        r_A = 0
+
 def main():
     puzzle_input = adventofcode.read_input(19)
     ip, program = parse_input(puzzle_input)
     adventofcode.answer(1, 1968, part1(ip, program))
-    #adventofcode.answer(2, 0, part2(puzzle_input, 50, 1000000000))
+    adventofcode.answer(2, 21211200, part2(True))
 
 if __name__ == '__main__':
     import doctest
