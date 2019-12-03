ca4ba553b66ac0d1e6ccf1a9cbf385a4c11f44b4
Keith Wissing <hitiek@xorad.com>
2019-12-03 01:04:06 -0500

    Year 2019 Day 3 Really ugly code. I may try to make it better later.

---
+++ 2019/day03.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import adventofcode
+
+def test1(lines):
+    """
+    >>> test1(['R8,U5,L5,D3', 'U7,R6,D4,L4'])
+    6
+    >>> test1(['R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D58,R83'])
+    159
+    >>> test1(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51','U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'])
+    135
+    """
+    tr = parse(lines)
+    return part1(tr)
+
+def test2(lines):
+    """
+    >>> test2(['R8,U5,L5,D3', 'U7,R6,D4,L4'])
+    30
+    >>> test2(['R75,D30,R83,U83,L12,D49,R71,U7,L72','U62,R66,U55,R34,D71,R55,D58,R83'])
+    610
+    >>> test2(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51','U98,R91,D20,R16,D67,R40,U7,R15,U6,R7'])
+    410
+    """
+    tr = parse(lines)
+    return part2(tr)
+
+def dist(a, b):
+    """
+    >>> dist((5, -5), (-5, 5))
+    20
+    """
+    return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def part1(traces):
+    grid = {}
+    for n, trace in enumerate(traces):
+        pos = (0, 0)
+        for (d, l) in trace:
+            change = {'R' : (1, 0), 'L' : (-1, 0), 'U' : (0, 1), 'D': (0, -1)}[d]
+            for _ in range(l):
+                pos = tuple(sum(x) for x in zip(pos, change))
+                if grid.get(pos, '.') == '.':
+                    grid[pos] = n
+                elif grid.get(pos) != n:
+                    grid[pos] = 'X'
+    md = 50000000
+    for i in grid.items():
+        if i[1] == 'X':
+            nd = dist((0, 0), i[0])
+            if nd < md:
+                md = nd
+    return md
+
+def trace_positions(trace):
+    pos = (0, 0)
+    for (d, l) in trace:
+        change = {'R' : (1, 0), 'L' : (-1, 0), 'U' : (0, 1), 'D': (0, -1)}[d]
+        for _ in range(l):
+            pos = tuple(sum(x) for x in zip(pos, change))
+            yield pos
+
+def part2(traces):
+    grid = {}
+    #print(list(zip(trace_positions(traces[0]), trace_positions(traces[1]))))
+    for n, trace in enumerate(traces):
+        pos = (0, 0)
+        cnt = 0
+        for (d, l) in trace:
+            change = {'R' : (1, 0), 'L' : (-1, 0), 'U' : (0, 1), 'D': (0, -1)}[d]
+            for _ in range(l):
+                pos = tuple(sum(x) for x in zip(pos, change))
+                cnt += 1
+                ov = grid.get(pos, (0, 0))
+                if ov[n] == 0 and n == 0:
+                    grid[pos] = (cnt, 0)
+                if ov[n] == 0 and n == 1:
+                    grid[pos] = (ov[0], cnt)
+    md = 50000000
+    for i in grid.values():
+        if i[0] != 0 and i[1] != 0:
+            nd = i[0] + i[1]
+            if nd < md:
+                md = nd
+    return md
+
+def parse(lines):
+    return [[(x[0], int(x[1:])) for x in l.split(',')] for l in lines]
+
+def main():
+    puzzle_input = adventofcode.read_input(3)
+    traces = parse(puzzle_input)
+    adventofcode.answer(1, 768, part1(traces))
+    adventofcode.answer(2, 8684, part2(traces))
+
+if __name__ == '__main__':
+    import doctest
+    doctest.testmod()
+    main()
