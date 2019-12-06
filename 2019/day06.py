2c8a66a70daf3c029aa79a9d0e8de1a5fcb3fa75
Keith Wissing <hitiek@xorad.com>
2019-12-06 01:03:34 -0500

    Refactor using generator and dictionary comprehension

--- 2019/day06.py
+++ 2019/day06.py
@@ -2,56 +2,34 @@
 
 import adventofcode
 
+def path(tree, s):
+    while s != 'COM':
+        yield s
+        s = tree[s]
+
 def orbital_checksum(orbits):
     """
     >>> orbital_checksum(['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L'])
     42
     """
-    tree = {}
-    for line in orbits:
-        s, m = line.split(')')
-        tree[m] = s
-    cnt = 0
-    for s in tree:
-        while s != 'COM':
-            cnt += 1
-            s = tree[s]
-    return cnt
+    tree = {m: s for s, m in [line.split(')') for line in orbits]}
+    return sum(1 for s in tree for _ in path(tree, s))
 
 def transfers(orbits):
     """
     >>> transfers(['COM)B', 'B)C', 'C)D', 'D)E', 'E)F', 'B)G', 'G)H', 'D)I', 'E)J', 'J)K', 'K)L', 'K)YOU', 'I)SAN'])
     4
     """
-    tree = {}
-    for line in orbits:
-        s, m = line.split(')')
-        tree[m] = s
-    cnts = {}
-    s = 'YOU'
-    cnt = 0
-    while s != 'COM':
-        cnts[s] = cnt
-        cnt += 1
-        s = tree[s]
-    s = 'SAN'
-    cnt = 0
-    while s != 'COM':
-        if s in cnts:
-            return cnt + cnts[s] - 2
-        cnt += 1
-        s = tree[s]
-
-def part1(orbits):
-    return orbital_checksum(orbits)
-
-def part2(orbits):
-    return transfers(orbits)
+    tree = {m: s for s, m in [line.split(')') for line in orbits]}
+    cnts = {n: c for c, n in enumerate(path(tree, 'YOU'))}
+    for c, n in enumerate(path(tree, 'SAN')):
+        if n in cnts:
+            return c + cnts[n] - 2
 
 def main():
     puzzle_input = adventofcode.read_input(6)
-    adventofcode.answer(1, 119831, part1(puzzle_input))
-    adventofcode.answer(2, 322, part2(puzzle_input))
+    adventofcode.answer(1, 119831, orbital_checksum(puzzle_input))
+    adventofcode.answer(2, 322, transfers(puzzle_input))
 
 if __name__ == '__main__':
     import doctest
