bcb9c4fec594dd10298135d5628830cc7ccc8264
Keith Wissing <hitiek@xorad.com>
2019-12-08 02:23:39 -0500

    refactored to use list comprehension

--- 2019/day08.py
+++ 2019/day08.py
@@ -3,44 +3,22 @@
 from collections import Counter
 import adventofcode
 
-def decode(image, width, height):
-    layersize = width * height
-    pos = 0
-    cz = 500000
-    while pos < len(image):
-        layer = image[pos:pos+layersize]
-        c = Counter(layer)
-        zeros = c[0]
-        if zeros < cz:
-            cz = zeros
-            ret = c[1]*c[2]
-        pos += layersize
-    return ret
-
-def part1(image):
-    return decode(image, 25, 6)
+def part1(image, width, height):
+    layers = [image[p:p+width*height] for p in range(0, len(image), width*height)]
+    data = {c[0]: c[1]*c[2] for c in [Counter(l) for l in layers]}
+    return data[min(data.keys())]
 
 def part2(image, width, height):
-    layersize = width * height
-    layers = []
-    pos = 0
-    while pos < len(image):
-        layers.append(image[pos:pos+layersize])
-        pos += layersize
-    for y in range(height):
-        for x in range(width):
-            for l in range(len(layers)):
-                p = layers[l][y*width+x]
-                if p != 2:
-                    print('#' if p == 1 else ' ', end = '')
-                    break
-        print()
+    layers = [image[p:p+width*height] for p in range(0, len(image), width*height)]
+    pixels = ['#' if next(x for x in l if x != 2) == 1 else ' ' for l in zip(*layers)]
+    rows = [''.join(pixels[p:p+width]) for p in range(0, width*height, width)]
+    print('\n'.join(rows))
     return 'GJYEA' # result of visual inspetion of the output
 
 def main():
     puzzle_input = adventofcode.read_input(8)
     puzzle_input = [int(x) for x in puzzle_input]
-    adventofcode.answer(1, 1742, part1(puzzle_input))
+    adventofcode.answer(1, 1742, part1(puzzle_input, 25, 6))
     adventofcode.answer(2, 'GJYEA', part2(puzzle_input, 25, 6))
 
 if __name__ == '__main__':
