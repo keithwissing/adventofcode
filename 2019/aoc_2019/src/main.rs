276c6851c415d5fbdcc52d385739e8293d09f048
Keith Wissing <hitiek@xorad.com>
2020-02-10 20:17:49 -0500

    Renamed some files

--- 2019/aoc_2019/src/main.rs
+++ 2019/aoc_2019/src/main.rs
@@ -1,26 +1,26 @@
 use std::time::Instant;
 mod day01;
-mod day02_hm;
-mod day02_s;
-mod day02_v;
+mod day02_hashmap;
+mod day02_slice;
+mod day02_vector;
 
 fn main() {
     day01::day01();
     {
         let now = Instant::now();
-        day02_hm::day02();
+        day02_hashmap::day02();
         let elapsed = now.elapsed();
         println!("HashMap : {:.2?}", elapsed);
     }
     {
         let now = Instant::now();
-        day02_v::day02();
+        day02_vector::day02();
         let elapsed = now.elapsed();
         println!("Vector : {:.2?}", elapsed);
     }
     {
         let now = Instant::now();
-        day02_s::day02();
+        day02_slice::day02();
         let elapsed = now.elapsed();
         println!("Slices : {:.2?}", elapsed);
     }
