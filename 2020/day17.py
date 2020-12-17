5307f362c08e3caaaba24745abbf85cb57c17afd
Keith Wissing <hitiek@xorad.com>
2020-12-17 01:36:59 -0500

    trying to create a dimension independent adjacent generator

--- 2020/day17.py
+++ 2020/day17.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 from collections import defaultdict
-import itertools
+from itertools import product
 import adventofcode
 
 t1 = [
@@ -27,24 +27,40 @@ def parse2(lines):
     return grid
 
 def adjacent(pos):
-    for d in itertools.product([-1, 0, 1], repeat=3):
+    if len(pos) == 3:
+        return adjacent3(pos)
+    if len(pos) == 4:
+        return adjacent4(pos)
+    return adjacent0(pos)
+
+def adjacent0(pos):
+    zero = tuple([0] * len(pos))
+    for delta in product([-1, 0, 1], repeat=len(pos)):
+        if delta == zero:
+            continue
+        yield tuple([a + b for a, b in zip(pos, delta)])
+        # yield tuple(map(sum, zip(pos, delta)))
+        # yield tuple(sum(x) for x in zip(pos, delta))
+
+def adjacent3(pos):
+    for d in product([-1, 0, 1], repeat=3):
         if d == (0, 0, 0):
             continue
         yield (pos[0] + d[0], pos[1] + d[1], pos[2] + d[2])
 
-def adjacent2(pos):
-    for d in itertools.product([-1, 0, 1], repeat=4):
+def adjacent4(pos):
+    for d in product([-1, 0, 1], repeat=4):
         if d == (0, 0, 0, 0):
             continue
         yield (pos[0] + d[0], pos[1] + d[1], pos[2] + d[2], pos[3] + d[3])
 
-def iterate(grid, adjfunc):
+def iterate(grid):
     ng = defaultdict(lambda: 0)
     mins = list(map(lambda x: x - 1, map(min, zip(*grid.keys()))))
     maxs = list(map(lambda x: x + 2, map(max, zip(*grid.keys()))))
     ranges = map(lambda t: range(t[0], t[1]), zip(mins, maxs))
-    for pos in itertools.product(*ranges):
-        nc = sum(grid[tp] for tp in adjfunc(pos))
+    for pos in product(*ranges):
+        nc = sum(grid[tp] for tp in adjacent(pos))
         if grid[pos] == 1 and nc in [2, 3]:
             ng[pos] = 1
         if grid[pos] == 0 and nc == 3:
@@ -58,7 +74,7 @@ def part1(lines):
     """
     grid = parse(lines)
     for _ in range(6):
-        grid = iterate(grid, adjacent)
+        grid = iterate(grid)
     return sum(grid.values())
 
 def part2(lines):
@@ -68,7 +84,7 @@ def part2(lines):
     """
     grid = parse2(lines)
     for _ in range(6):
-        grid = iterate(grid, adjacent2)
+        grid = iterate(grid)
     return sum(grid.values())
 
 def main():
