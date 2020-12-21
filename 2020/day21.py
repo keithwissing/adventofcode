b375661361266d8dd42d034e1f0613b659b6e5a3
Keith Wissing <hitiek@xorad.com>
2020-12-21 01:27:38 -0500

    day 21 break part 2 into functions

--- 2020/day21.py
+++ 2020/day21.py
@@ -17,6 +17,41 @@ def parse(lines):
         foods.append([m[1].split(), m[2].split(', ')])
     return foods
 
+def find_safe(foods, all_ingredients, all_allergens):
+    safe = set(all_ingredients)
+    for a in all_allergens:
+        p = set(all_ingredients)
+        for f in foods:
+            if a in f[1]:
+                p &= set(f[0])
+        safe = safe.difference(p)
+    return safe
+
+def find_possible_ingredients_for_allergens(foods, all_allergens, danger):
+    could = {}
+    for a in all_allergens:
+        p = set(danger)
+        for f in foods:
+            if a in f[1]:
+                p = p.intersection(f[0])
+        could[a] = list(p)
+    return could
+
+def reduce_possibilities(could):
+    remaining = {k: v[:] for k, v in could.items()}  # don't change the input
+    changed = True
+    while changed:
+        changed = False
+        for k, vs in remaining.items():
+            if len(vs) == 1:
+                v = vs[0]
+                for k2, v2 in remaining.items():
+                    if k != k2 and v in v2:
+                        v2.remove(v)
+                        remaining[k2] = v2
+                        changed = True
+    return {k: vs.pop() for k, vs in remaining.items()}
+
 def part1(lines):
     """
     >>> part1(t1)
@@ -25,13 +60,7 @@ def part1(lines):
     foods = parse(lines)
     all_ingredients = set(a for f in foods for a in f[0])
     all_allergens = set(a for f in foods for a in f[1])
-    safe = set(all_ingredients)
-    for a in all_allergens:
-        p = set(all_ingredients)
-        for f in foods:
-            if a in f[1]:
-                p &= set(f[0])
-        safe = safe.difference(p)
+    safe = find_safe(foods, all_ingredients, all_allergens)
     return sum(len(set(f[0]).intersection(safe)) for f in foods)
 
 def part2(lines):
@@ -42,38 +71,13 @@ def part2(lines):
     foods = parse(lines)
     all_ingredients = set(a for f in foods for a in f[0])
     all_allergens = set(a for f in foods for a in f[1])
-    safe = set(all_ingredients)
-    for a in all_allergens:
-        p = set(all_ingredients)
-        for f in foods:
-            if a in f[1]:
-                p &= set(f[0])
-        safe = safe.difference(p)
-
-    for f in foods:
-        f[0] = [x for x in f[0] if x not in safe]
+    safe = find_safe(foods, all_ingredients, all_allergens)
 
     danger = all_ingredients.difference(safe)
-    could = {}
-    for a in all_allergens:
-        p = set(danger)
-        for f in foods:
-            if a in f[1]:
-                p = p.intersection(f[0])
-        could[a] = list(p)
-    changed = True
-    while changed:
-        changed = False
-        for k, vs in could.items():
-            if len(vs) == 1:
-                v = vs[0]
-                for k2, v2 in could.items():
-                    if k != k2 and v in v2:
-                        v2.remove(v)
-                        could[k2] = v2
-                        changed = True
-    a = ','.join(could[k].pop() for k in sorted(could.keys()))
-    return a
+    could = find_possible_ingredients_for_allergens(foods, all_allergens, danger)
+
+    results = reduce_possibilities(could)
+    return ','.join(results[k] for k in sorted(results.keys()))
 
 def main():
     puzzle_input = adventofcode.read_input(21)
