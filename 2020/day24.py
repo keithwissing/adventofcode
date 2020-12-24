6ff114256ca9e2d0a648bb091e76aac0f25ef842
Keith Wissing <hitiek@xorad.com>
2020-12-24 12:22:43 -0500

    refactored day 24

--- 2020/day24.py
+++ 2020/day24.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 
-from collections import defaultdict
+from collections import Counter, defaultdict
 from itertools import product
+import re
 import adventofcode
 
 t1 = [
@@ -28,27 +29,15 @@ t1 = [
 ]
 
 def coordinate_of(line):
-    pos = 0
-    dirs = {d: 0 for d in ['e', 'se', 'sw', 'w', 'nw', 'ne']}
-    while pos < len(line):
-        if line[pos] in ['e', 'w']:
-            dirs[line[pos]] += 1
-            pos += 1
-        elif line[pos:pos + 2] in ['se', 'sw', 'ne', 'nw']:
-            dirs[line[pos:pos + 2]] += 1
-            pos += 2
-    vec = (dirs['e'] - dirs['w'], dirs['ne'] - dirs['sw'], dirs['nw'] - dirs['se'])
-    hid = (vec[0] - vec[2], vec[1] + vec[2])
-    return hid
+    # using coordinates like http://devmag.org.za/blog/wp-content/uploads/2013/08/screen_122.png
+    dirs = Counter(re.findall(r'e|se|sw|w|nw|ne', line))
+    vector = (dirs['e'] - dirs['w'], dirs['ne'] - dirs['sw'], dirs['nw'] - dirs['se'])
+    coordinate = (vector[0] - vector[2], vector[1] + vector[2])
+    return coordinate
 
 def starting_floor(lines):
-    floor = {}
-    for line in lines:
-        hid = coordinate_of(line)
-        if hid in floor:
-            floor[hid] = 1 - floor[hid]
-        else:
-            floor[hid] = 1
+    tiles = Counter(coordinate_of(l) for l in lines)
+    floor = defaultdict(lambda: 0, {c: 1 for c in tiles if tiles[c] % 2 == 1})
     return floor
 
 def part1(lines):
@@ -60,8 +49,7 @@ def part1(lines):
     return sum(floor.values())
 
 def adjacent(pos):
-    ds = ((-1, 1), (0, 1), (1, 0), (1, -1), (0, -1), (-1, 0))
-    for d in ds:
+    for d in ((-1, 1), (0, 1), (1, 0), (1, -1), (0, -1), (-1, 0)):
         yield (pos[0] + d[0], pos[1] + d[1])
 
 def iterate(floor):
@@ -70,7 +58,7 @@ def iterate(floor):
     ranges = map(lambda t: range(t[0], t[1]), limits)
     for pos in product(*ranges):
         nei = sum(floor[a] for a in adjacent(pos))
-        if floor[pos] and nei in [1,2]:
+        if floor[pos] and nei in [1, 2]:
             nf[pos] = 1
         if floor[pos] == 0 and nei == 2:
             nf[pos] = 1
@@ -81,7 +69,7 @@ def part2(lines):
     >>> part2(t1)
     2208
     """
-    floor = defaultdict(lambda: 0, starting_floor(lines))
+    floor = starting_floor(lines)
     for _ in range(100):
         floor = iterate(floor)
     return sum(floor.values())
