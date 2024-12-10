6610d3d2d44991e82d3cb9d352b18496146841c7
Keith Wissing <hitiek@xorad.com>
2024-12-10 00:25:09 -0500

    year 2024 day 10 refactor

--- 2024/day10.py
+++ 2024/day10.py
@@ -18,15 +18,12 @@ t1 = [
 def add(pos, delta):
     return pos[0] + delta[0], pos[1] + delta[1]
 
-def part1(lines):
-    """
-    >>> part1(t1)
-    36
-    """
+def both(lines):
     width, height = len(lines[0]), len(lines)
     grid = defaultdict(lambda: '', {(x, y): int(lines[y][x]) for x in range(width) for y in range(height)})
 
-    total = 0
+    score = 0
+    rating = 0
 
     deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
 
@@ -39,42 +36,30 @@ def part1(lines):
                 for d in deltas:
                     if grid[add(pos, d)] == h + 1:
                         if h + 1 == 9:
+                            rating += 1
                             peaks.add(add(pos, d))
                         else:
                             steps.append((add(pos, d), h + 1))
-            total += len(peaks)
+            score += len(peaks)
 
-    return total
+    return score, rating
+
+def part1(lines):
+    """
+    >>> part1(t1)
+    36
+    """
+    return both(lines)[0]
 
 def part2(lines):
     """
     >>> part2(t1)
     81
     """
-    width, height = len(lines[0]), len(lines)
-    grid = defaultdict(lambda: '', {(x, y): int(lines[y][x]) for x in range(width) for y in range(height)})
-
-    total = 0
-
-    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
-
-    for x, y in product(range(width), range(height)):
-        if grid[(x, y)] == 0:
-            steps = [((x, y), 0)]
-            while steps:
-                pos, h = steps.pop()
-                for d in deltas:
-                    if grid[add(pos, d)] == h + 1:
-                        if h + 1 == 9:
-                            total += 1
-                        else:
-                            steps.append((add(pos, d), h + 1))
-
-    return total
+    return both(lines)[1]
 
 def main():
     puzzle_input = adventofcode.read_input(10)
-    # puzzle_input = [int(x) for x in puzzle_input]
     adventofcode.answer(1, 754, part1(puzzle_input))
     adventofcode.answer(2, 1609, part2(puzzle_input))
 
